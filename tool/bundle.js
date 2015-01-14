da588046d2feaf2be6bae6865184c69ea8b94147
Vince Speelman <v@vinspee.me>
2015-01-14 09:39:53 -0500

    mod

--- tool/bundle.js
+++ tool/bundle.js
@@ -3,7 +3,7 @@ var source     = require('vinyl-transform');
 
 module.exports = function(t) {
 	var b = browserify();
-	b.add(t.source(t.path[0]));
+	b.add(t.src(t.path[0]));
 
 	t.build(b.bundle(), t.source(t.path[1]), t.dest());
 }
--- tool/index.js
+++ tool/index.js
@@ -4,6 +4,6 @@ var concat = require('gulp-concat');
 chron('default', chron.once('bundle'));
 
 chron('bundle', chron.once('concat')
-	.path('../app/scripts/**/*.js', 'bundle.js')
+	.path('../app/scripts/**/*.js', 'dude.js')
 	.dest('./ex-test.js'),
 	require('./bundle.js'));
